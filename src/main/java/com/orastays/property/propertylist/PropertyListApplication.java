8b6a4f94cf27cd89f81df6fd2210223a37755bf2
Avirup.Pal <Avirup.Pal@cognizant.com>
2018-12-01 22:53:17 +0530

    first commit

---
+++ src/main/java/com/orastays/property/propertylist/PropertyListApplication.java
@@ -0,0 +1,12 @@
+package com.orastays.property.propertylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyListApplication.class, args);
+	}
+}
