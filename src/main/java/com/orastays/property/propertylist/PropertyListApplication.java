d649ecb8ff53971ac91b3252bec76fc3e8d52896
avi3s <avirup.pal@gmail.com>
2018-12-14 03:03:50 +0530

    gradle changes

--- src/main/java/com/orastays/property/propertylist/PropertyListApplication.java
+++ src/main/java/com/orastays/property/propertylist/PropertyListApplication.java
@@ -5,59 +5,50 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.filter.ForwardedHeaderFilter;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@EnableResourceServer
-//@EnableEurekaClient
-@RestController
+//@EnableResourceServer
+//@EnableDiscoveryClient
 @EnableAutoConfiguration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableCircuitBreaker
+//@EnableHystrix
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class PropertyListApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PropertyListApplication.class, args);
 	}
 	
-	@Bean
-	FilterRegistrationBean forwardedHeaderFilter() {
-		FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
-		filterRegBean.setFilter(new ForwardedHeaderFilter());
-		filterRegBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return filterRegBean;
-	}
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login");
-		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
-	}
-
-	@Bean
-	public FilterRegistrationBean corsFilters() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
-	}
+//	@Bean
+//	FilterRegistrationBean forwardedHeaderFilter() {
+//		FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
+//		filterRegBean.setFilter(new ForwardedHeaderFilter());
+//		filterRegBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//		return filterRegBean;
+//	}
+//
+//	@Override
+//	public void addViewControllers(ViewControllerRegistry registry) {
+//		registry.addViewController("/login").setViewName("login");
+//		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+//	}
+//
+//	@Bean
+//	public FilterRegistrationBean corsFilters() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowCredentials(true);
+//		config.addAllowedOrigin("*");
+//		config.addAllowedHeader("*");
+//		config.addAllowedMethod("*");
+//		source.registerCorsConfiguration("/**", config);
+//		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+//		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+//		return bean;
+//	}
 	
 	@Bean
     public ModelMapper modelMapper() {
