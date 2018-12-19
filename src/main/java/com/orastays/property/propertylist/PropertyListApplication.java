8df3c58d3876afc723b5fb700c784b7916ecbc4f
avi3s <avirup.pal@gmail.com>
2018-12-20 03:08:33 +0530

    build.gradle

--- src/main/java/com/orastays/property/propertylist/PropertyListApplication.java
+++ src/main/java/com/orastays/property/propertylist/PropertyListApplication.java
@@ -1,65 +1,87 @@
 package com.orastays.property.propertylist;
 
+import java.util.List;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-//@EnableResourceServer
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableAutoConfiguration
-//@EnableCircuitBreaker
-//@EnableHystrix
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableCircuitBreaker
+@EnableHystrix
 public class PropertyListApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PropertyListApplication.class, args);
 	}
 	
-//	@Bean
-//	FilterRegistrationBean forwardedHeaderFilter() {
-//		FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
-//		filterRegBean.setFilter(new ForwardedHeaderFilter());
-//		filterRegBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//		return filterRegBean;
-//	}
-//
-//	@Override
-//	public void addViewControllers(ViewControllerRegistry registry) {
-//		registry.addViewController("/login").setViewName("login");
-//		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
-//	}
-//
-//	@Bean
-//	public FilterRegistrationBean corsFilters() {
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		CorsConfiguration config = new CorsConfiguration();
-//		config.setAllowCredentials(true);
-//		config.addAllowedOrigin("*");
-//		config.addAllowedHeader("*");
-//		config.addAllowedMethod("*");
-//		source.registerCorsConfiguration("/**", config);
-//		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-//		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//		return bean;
-//	}
-	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/test/**").addResourceLocations("/test/").setCachePeriod(0);
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(0);
+        registry.addResourceHandler("/img/**").addResourceLocations("/img/").setCachePeriod(0);
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(0);
+    }
+
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        converters.add(new MappingJackson2HttpMessageConverter());
+        super.configureMessageConverters(converters);
+    }
+
 	@Bean
-    public ModelMapper modelMapper() {
-		
+	public ModelMapper modelMapper() {
+
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT).setAmbiguityIgnored(true);
 		return modelMapper;
 	}
-	
+
 	@Bean
-    public RestTemplate restTemplate() {
+	@LoadBalanced
+	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+	
+	//Tomcat large file upload connection reset
+    @Bean
+    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+
+        return tomcat;
+    }
+    
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(-1);
+        return multipartResolver;
+    }
 }
